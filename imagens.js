4e8fec5a77098042bd7bcdd2f03b8705ee059fbf
joseheb123 <joseheb123@gmail.com>
2025-11-29 11:48:12 -0300

    Update imagens.js

--- imagens.js
+++ imagens.js
@@ -1,15 +1,3 @@
 [
-  { "url": "https://i.imgur.com/TFj8JZ8.png", "chunk": [757, 1162], "coords": [79, 232] },
-  { "url": "https://i.imgur.com/oI1HPYI.png", "chunk": [761, 1158], "coords": [713, 699] },
-  { "url": "https://i.ibb.co/nqPVZRKJ/TEMPLATENOVO.png", "chunk": [754, 1156], "coords": [195, 465] },
-  { "url": "https://i.imgur.com/oNdHp5a.png", "chunk": [777, 1153], "coords": [204, 541] },
-  { "url": "https://i.imgur.com/aGKmeNy.png", "chunk": [762, 1165], "coords": [105, 521] },
-  { "url": "https://i.imgur.com/FwZHDQx.png", "chunk": [762, 1179], "coords": [866, 192] },
-  { "url": "https://i.imgur.com/xz30He0.png", "chunk": [755, 1162], "coords": [551, 838] },
-  { "url": "https://i.imgur.com/rEp12Wz.png", "chunk": [757, 1162], "coords": [102, 901] },
-  { "url": "https://i.imgur.com/dnqVhmq.png", "chunk": [756, 1162], "coords": [885, 661] },
-  { "url": "https://i.imgur.com/fqeR2Vo.png", "chunk": [756, 1161], "coords": [602, 942] },
-  { "url": "https://i.imgur.com/A6oUTlJ.png", "chunk": [757, 1162], "coords": [480, 553] },
-  { "url": "https://i.imgur.com/Hmw0cBk.png", "chunk": [1095, 675], "coords": [1, 0] },
-  { "url": "https://i.imgur.com/3vx3nFm.png", "chunk": [756, 1164], "coords": [770, 791] }
+  { "url": "https://i.imgur.com/6C4Kwav.png", "chunk": [973, 758], "coords": [0, 0] }
 ]
